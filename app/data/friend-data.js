ea2cd8dd7d750c1a2e80e6f56b4dae101784fdff
LouiseKMiller <louise.austex@gmail.com>
2016-07-22 19:41:44 -0500

    working set

--- app/data/friend-data.js
+++ app/data/friend-data.js
@@ -8,30 +8,34 @@
 var friendsArray = [
 		{
 		name: "Opus",
-		photoURL: "xxx",
+		photoURL: "http://i.ebayimg.com/images/g/J1wAAOSwpDdVbZvG/s-l300.jpg",
 		answers: ["0", "3"],
 	},
 	{
 		name: "Steve Dallas",
-		photoURL: "xxx",
+		photoURL: "https://en.gravatar.com/userimage/7142978/18bd423b0c3d1d2c6597b618e72bea91.jpg?size=200",
 		answers: ["0", "0"],
 	},
 	{
 		name: "Bill the Cat",
-		photoURL: "xxx",
+		photoURL: "https://s-media-cache-ak0.pinimg.com/736x/be/10/ef/be10ef819e52bdda00c75b6a9f037f2d.jpg",
 		answers: ["1", "1"],
 	},
 	{
-		name: "Boopsie",
-		photoURL: "xxx",
+		name: "Binkley",
+		photoURL: "http://www.jrj-socrates.com/Cartoon%20Pics/Comics/Bloom%20County/Binkley_300.gif",
 		answers: ["2", "2"],
 	},
 	{
-		name: "Cutter John",
-		photoURL: "xxx",
+		name: "Milo",
+		photoURL: "https://s-media-cache-ak0.pinimg.com/736x/74/07/7a/74077a755e37aada5bb2e008fa017258.jpg",
 		answers: ["3", "3"],
 	},
-
+	{
+		name: "Portnoy",
+		photoURL: "http://www.jrj-socrates.com/Cartoon%20Pics/Comics/Bloom%20County/Portnoy_300.gif",
+		answers: ["3", "3"],
+	}
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
